f17615e9859b60a5447743b67532890c14d22a90
peiwand1 <49898715+peiwand1@users.noreply.github.com>
2024-10-17 14:13:50 +0200

    added math functions

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -1,21 +1,38 @@
 package calculator;
 
 import calculator.formatter.Formatter;
+import java.lang.Math;
 
 public class Calculator {
-    private Formatter formatter;
+	private Formatter formatter;
 
-    // constructor
-    public Calculator(Formatter formatter) {
-        this.formatter = formatter;
+	// constructor
+	public Calculator(Formatter formatter) {
+		this.formatter = formatter;
+	}
+
+	public double add(double a, double b) {
+		return a + b;
+	}
+
+	public double subtract(double a, double b) {
+		return a - b;
+	}
+
+	public double divide(double a, double b) {
+		return a / b;
+	}
+
+	public double multiply(double a, double b) {
+		return a * b;
+	}
+	
+	public double power(double a, double b) {
+        return Math.pow(a, b);
     }
 
-    public double add(double a, double b) {
-        return a + b;
-    }
-
-    // return result based on formatter
-    public String getFormattedResult(double result) {
-        return formatter.format(result);
-    }
+	// return result based on formatter
+	public String getFormattedResult(double result) {
+		return formatter.format(result);
+	}
 }
